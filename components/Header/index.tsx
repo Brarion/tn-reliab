0787493c4a054e80c1861883c5b5a2287141e8e3
voronkova.my <marina.v18@yandex.ru>
2021-11-24 13:58:40 +0300

    add Header

---
+++ components/Header/index.tsx
@@ -0,0 +1,36 @@
+import Image from 'next/image'
+
+import styles from './styles.module.scss'
+
+const Header = () => {
+  return (
+    <div className={styles.wrapper}>
+      <header>
+        <Image src={'/assets/logo.svg'} alt={'Технологии надежности'} width={128} height={40} />
+        <ul>
+          <li>
+            <a href={'/main'}>Главная</a>
+          </li>
+          <Image className={styles.ellipse} alt={''} src={'/assets/ellipse.svg'} width={4} height={4} />
+          <li>
+            <a href={'/about-as'}>О нас</a>
+          </li>
+          <Image className={styles.ellipse} alt={''} src={'/assets/ellipse.svg'} width={4} height={4} />
+          <li>
+            <a href={'/blog'}>Блог</a>
+          </li>
+          <Image className={styles.ellipse} alt={''} src={'/assets/ellipse.svg'} width={4} height={4} />
+          <li>
+            <a href={'/contacts'}>Контакты</a>
+          </li>
+          <Image className={styles.ellipse} alt={''} src={'/assets/ellipse.svg'} width={4} height={4} />
+          <li>
+            <a href={'/vacancy'}>Вакансии</a>
+          </li>
+        </ul>
+      </header>
+    </div>
+  )
+}
+
+export default Header
