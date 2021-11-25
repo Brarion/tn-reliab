70ad25ae1e1d2f6b8b52272488045f7778034ad0
voronkova.my <marina.v18@yandex.ru>
2021-11-25 16:26:45 +0300

    add Company

---
+++ components/MainPage/components/GroupCompany/Company/index.tsx
@@ -0,0 +1,43 @@
+import Image from 'next/image'
+import styles from './styles.module.scss'
+
+type Props = {
+  image: string
+  header: string
+  text: string
+  countEmployees: number
+  countProjects: number
+  textEmployees: string
+  textProjects: string
+}
+
+const Company = (props: Props) => {
+  const { image, header, text, countEmployees, countProjects, textEmployees, textProjects } = props
+  return (
+    <article className={styles.company}>
+      <Image src={image} alt={header} width={77} height={77} />
+      <div className={styles.companyInfo}>
+        <h2>{header}</h2>
+        <p>{text}</p>
+        <div className={styles.additionalInfo}>
+          <button>СВЯЗАТЬСЯ</button>
+          <div className={styles.line} />
+          <div className={styles.stuff}>
+            <Image src={'/assets/stuff.svg'} alt={''} width={14} height={15} />
+            <p>
+              {countEmployees} {textEmployees}
+            </p>
+          </div>
+          <div className={styles.projects}>
+            <Image src={'/assets/project.svg'} alt={''} width={14} height={15} />
+            <p>
+              {countProjects} {textProjects}
+            </p>
+          </div>
+        </div>
+      </div>
+    </article>
+  )
+}
+
+export default Company
