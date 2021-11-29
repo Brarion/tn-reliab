ef22b85abb145c4b81359d73c1090fd177841832
voronkova.my <marina.v18@yandex.ru>
2021-11-29 15:38:53 +0300

    add Jobs and Contacts

--- components/MainPage/index.tsx
+++ components/MainPage/index.tsx
@@ -1,6 +1,8 @@
 import Header from '@components/Header'
 import Jumbotron from '@components/MainPage/components/Jumbotron'
 import GroupCompany from '@components/MainPage/components/GroupCompany'
+import Jobs from '@components/MainPage/components/Jobs'
+import Contacts from '@components/MainPage/components/Contacts'
 
 const MainPage = () => {
   return (
@@ -8,6 +10,8 @@ const MainPage = () => {
       <Header />
       <Jumbotron />
       <GroupCompany />
+      <Jobs />
+      <Contacts />
     </div>
   )
 }
