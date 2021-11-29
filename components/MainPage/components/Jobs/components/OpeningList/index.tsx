d8bc6c9ddd429f272f704dd78ae6b4ae4273cd80
voronkova.my <marina.v18@yandex.ru>
2021-11-29 15:34:23 +0300

    add OpeningList

---
+++ components/MainPage/components/Jobs/components/OpeningList/index.tsx
@@ -0,0 +1,33 @@
+import styles from './styles.module.scss'
+
+type Props = {
+  header: string
+  items: Array<string>
+  opened: Boolean
+  onClick: (jobId: number) => void
+  id: number
+}
+
+const OpeningList = (props: Props) => {
+  const { header, items, opened, onClick, id } = props
+  return (
+    <div className={styles.wrapper}>
+      <div className={`${styles.header} ${opened ? styles.opening : ''}`} onClick={() => onClick(id)}>
+        <h1>{header}</h1>
+        <button>
+          <div />
+          <div />
+        </button>
+      </div>
+      <div className={styles.horizontalLine} />
+      <ul className={opened ? styles.opening : ''} id={id.toString()}>
+        {items.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+        <div className={styles.horizontalLine} />
+      </ul>
+    </div>
+  )
+}
+
+export default OpeningList
