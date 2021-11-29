48115e97c1b945cb1c7b6829c24e8d838c5ad133
voronkova.my <marina.v18@yandex.ru>
2021-11-29 17:59:33 +0300

    add onClick in Button

--- components/Button/index.tsx
+++ components/Button/index.tsx
@@ -3,10 +3,16 @@ import styles from './styles.module.scss'
 type Props = {
   text: string
   className: string
+  onClick: () => void
+  type: 'submit' | 'reset' | 'button' | undefined
 }
 
-const Button = ({ text, className }: Props) => {
-  return <button className={`${styles.button} ${className}`}>{text}</button>
+const Button = ({ text, className, onClick, type }: Props) => {
+  return (
+    <button className={`${styles.button} ${className}`} onClick={onClick} type={type}>
+      {text}
+    </button>
+  )
 }
 
 export default Button
