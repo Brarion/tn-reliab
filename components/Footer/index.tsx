e283b0b64e34425f04e34f478150e8a929530fd8
voronkova.my <marina.v18@yandex.ru>
2021-12-01 18:26:51 +0300

    add adaptive version

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -7,31 +7,34 @@ const Footer = () => {
     <div className={styles.wrapper}>
       <footer>
         <div className={styles.content}>
-          <h2>Группа компаний «Технологии надёжности»</h2>
+          <h2>
+            <span>Группа компаний&nbsp;</span>
+            <span>«Технологии надёжности»</span>
+          </h2>
           <div className={styles.mainPart}>
             <div className={styles.info}>
               <div className={styles.item}>
-                <Image src={'/assets/pirsbw.svg'} alt={''} width={29} height={30} />
+                <Image src={'/assets/pirsbw.svg'} alt={'Пирс'} width={29} height={30} />
                 <p>Пирс</p>
               </div>
               <div className={styles.item}>
-                <Image src={'/assets/skypowerbw.svg'} alt={''} width={30} height={30} />
+                <Image src={'/assets/skypowerbw.svg'} alt={'Skypower'} width={30} height={30} />
                 <p>Skypower</p>
               </div>
               <div className={styles.item}>
-                <Image src={'/assets/kvantabw.svg'} alt={''} width={25} height={30} />
+                <Image src={'/assets/kvantabw.svg'} alt={'KVANTA'} width={25} height={30} />
                 <p>KVANTA</p>
               </div>
               <div className={styles.item}>
-                <Image src={'/assets/elementbw.svg'} alt={''} width={30} height={30} />
+                <Image src={'/assets/elementbw.svg'} alt={'Пятый элемент'} width={30} height={30} />
                 <p>Пятый элемент</p>
               </div>
               <div className={styles.item}>
-                <Image src={'/assets/expertChoicebw.svg'} alt={''} width={22} height={30} />
+                <Image src={'/assets/expertChoicebw.svg'} alt={'Expert Choice CIS'} width={22} height={30} />
                 <p>Expert Choice CIS</p>
               </div>
               <div className={styles.item}>
-                <Image src={'/assets/geoexpertbw.svg'} alt={''} width={29} height={30} />
+                <Image src={'/assets/geoexpertbw.svg'} alt={'Geoexpert'} width={29} height={30} />
                 <p>Geoexpert</p>
               </div>
             </div>
@@ -49,7 +52,10 @@ const Footer = () => {
             </div>
           </div>
         </div>
-        <div className={styles.underfoot}>© «Технологии надежности» 2021. Все права защищены</div>
+        <div className={styles.underfoot}>
+          <span>© «Технологии надежности» 2021.&nbsp;</span>
+          <span>Все права защищены</span>
+        </div>
       </footer>
     </div>
   )
