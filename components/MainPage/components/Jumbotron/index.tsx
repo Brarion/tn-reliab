7bdf1ab43b5825259a92491cf270be84f7b7ae67
voronkova.my <marina.v18@yandex.ru>
2021-12-01 10:03:36 +0300

    add background

--- components/MainPage/components/Jumbotron/index.tsx
+++ components/MainPage/components/Jumbotron/index.tsx
@@ -1,14 +1,28 @@
 import Image from 'next/image'
 import Button from '@components/Button'
 import styles from './styles.module.scss'
+import React from 'react'
+
+import Background1 from '@public/assets/background1.svg'
+import Background2 from '@public/assets/background2.svg'
+import Background3 from '@public/assets/background3.svg'
 
 const Jumbotron = () => {
-  const onclick = () => {
+  const handleClick = () => {
     // Перейти на Contacts
   }
 
   return (
     <div className={styles.wrapper}>
+      <div className={styles.imgWrapper1}>
+        <Image src={Background1} alt={''} width={175} height={175} />
+      </div>
+      <div className={styles.imgWrapper2}>
+        <Image src={Background2} alt={''} width={176} height={176} />
+      </div>
+      <div className={styles.imgWrapper3}>
+        <Image src={Background3} alt={''} width={466} height={466} />
+      </div>
       <div className={styles.mainContent}>
         <div className={styles.leftSide}>
           <div className={styles.text}>
@@ -17,14 +31,14 @@ const Jumbotron = () => {
               {`Уделяем особое внимание проектированию интерфейсов,
               в результате чего мы с горостью можем назвать наши продукты Надёжными!`}
             </p>
-            <Button text={'старт с нами'} className={''} onClick={onclick} type={'button'} />
+            <Button text={'старт с нами'} className={''} onClick={handleClick} type={'button'} />
           </div>
           <aside className={styles.devInfo}>
             <div className={styles.line} />
             {`Сайт находится в режиме разработки.\nБудем рады ответить на Ваши вопросы`}
           </aside>
         </div>
-        <Image alt={''} src={'/assets/phone.png'} width={700} height={700} priority />
+        <Image alt={'Технологии надёжности'} src={'/assets/phone.png'} width={700} height={700} priority />
       </div>
       <div className={styles.information}>
         <div className={styles.contacts}>
