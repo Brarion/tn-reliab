2f17b6fac2f693691ef59de3af354a067242c540
voronkova.my <marina.v18@yandex.ru>
2021-12-01 16:58:52 +0300

    add adaptive version

--- components/MainPage/components/Contacts/index.tsx
+++ components/MainPage/components/Contacts/index.tsx
@@ -1,11 +1,13 @@
 import Image from 'next/image'
 import Contact from '@components/Contact'
 import React from 'react'
+import useResize from '../../../../utils/useResize'
 import styles from './styles.module.scss'
 import Background1 from '@public/assets/background5.svg'
 import Background2 from '@public/assets/background6.svg'
 
 const Contacts = () => {
+  const width = useResize()
   return (
     <div className={styles.wrapper}>
       <div className={styles.imgWrapper1}>
@@ -21,18 +23,35 @@ const Contacts = () => {
             Мы проверяем почту кажый час и с радостью ответим на любой Ваш вопрос. Наш менеджер свяжется со
             специалистами, чтобы вы получили самый компетентный ответ
           </p>
-          <div className={styles.maps}>
-            <a className={styles.item} href={'https://goo.gl/maps/VUyGjbXkc4zgT8c46'} rel="noreferrer" target="_blank">
-              <Image src={'/assets/map1.svg'} alt={'Google Maps'} width={110} height={23} />
-            </a>
-            <a className={styles.item} href={'https://go.2gis.com/5z7ask'} rel="noreferrer" target="_blank">
-              <Image src={'/assets/map2.svg'} alt={'2GIS'} width={54} height={23} />
-            </a>
-          </div>
+          {width >= 915 && (
+            <div className={styles.maps}>
+              <a
+                className={styles.item}
+                href={'https://goo.gl/maps/VUyGjbXkc4zgT8c46'}
+                rel="noreferrer"
+                target="_blank"
+              >
+                <Image src={'/assets/map1.svg'} alt={'Google Maps'} width={110} height={23} />
+              </a>
+              <a className={styles.item} href={'https://go.2gis.com/5z7ask'} rel="noreferrer" target="_blank">
+                <Image src={'/assets/map2.svg'} alt={'2GIS'} width={54} height={23} />
+              </a>
+            </div>
+          )}
         </div>
         <div className={styles.right}>
           <Contact />
         </div>
+        {width < 915 && (
+          <div className={styles.maps}>
+            <a className={styles.item} href={'https://goo.gl/maps/VUyGjbXkc4zgT8c46'} rel="noreferrer" target="_blank">
+              <Image src={'/assets/map1.svg'} alt={'Google Maps'} width={184} height={38} />
+            </a>
+            <a className={styles.item} href={'https://go.2gis.com/5z7ask'} rel="noreferrer" target="_blank">
+              <Image src={'/assets/map2.svg'} alt={'2GIS'} width={89} height={38} />
+            </a>
+          </div>
+        )}
       </div>
     </div>
   )
