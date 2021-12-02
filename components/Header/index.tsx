8ca82ee8fd86649627532257d39428ac52220086
voronkova.my <marina.v18@yandex.ru>
2021-12-02 15:36:55 +0300

    edit Header

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,33 +1,39 @@
 import Image from 'next/image'
 
 import styles from './styles.module.scss'
+import useResize from '../../utils/useResize'
 
 const Header = () => {
+  const width = useResize()
+
   return (
     <div className={styles.wrapper}>
       <header>
         <Image src={'/assets/logo.svg'} alt={'Технологии надежности'} width={128} height={40} />
-        <ul>
-          <li>
-            <a href={'/main'}>Главная</a>
-          </li>
-          <Image alt={''} src={'/assets/ellipse.svg'} width={4} height={4} />
-          <li>
-            <a href={'/about-as'}>О нас</a>
-          </li>
-          <Image alt={''} src={'/assets/ellipse.svg'} width={4} height={4} />
-          <li>
-            <a href={'/blog'}>Блог</a>
-          </li>
-          <Image alt={''} src={'/assets/ellipse.svg'} width={4} height={4} />
-          <li>
-            <a href={'/contacts'}>Контакты</a>
-          </li>
-          <Image alt={''} src={'/assets/ellipse.svg'} width={4} height={4} />
-          <li>
-            <a href={'/vacancy'}>Вакансии</a>
-          </li>
-        </ul>
+        {width > 768 && (
+          <ul>
+            <li>
+              <a href={'/main'}>Главная</a>
+            </li>
+            <Image alt={''} src={'/assets/ellipse.svg'} width={4} height={4} />
+            <li>
+              <a href={'/about-as'}>О нас</a>
+            </li>
+            <Image alt={''} src={'/assets/ellipse.svg'} width={4} height={4} />
+            <li>
+              <a href={'/blog'}>Блог</a>
+            </li>
+            <Image alt={''} src={'/assets/ellipse.svg'} width={4} height={4} />
+            <li>
+              <a href={'/contacts'}>Контакты</a>
+            </li>
+            <Image alt={''} src={'/assets/ellipse.svg'} width={4} height={4} />
+            <li>
+              <a href={'/vacancy'}>Вакансии</a>
+            </li>
+          </ul>
+        )}
+        {width <= 768 && <div className={styles.menu}></div>}
       </header>
     </div>
   )
