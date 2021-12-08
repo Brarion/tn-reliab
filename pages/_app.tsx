4f2cdaf0caeff76b1de11d78c0d1accb8f0d289f
voronkova.my <marina.v18@yandex.ru>
2021-12-08 16:06:19 +0300

    fix fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,6 +11,12 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Head>
         <title>Технологии надежности</title>
         <link rel="icon" href="/favicon.svg" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <Component {...pageProps} />
     </html>
