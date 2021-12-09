b2d4446021e4886b357a475d2d187c659999a573
voronkova.my <marina.v18@yandex.ru>
2021-12-09 09:50:52 +0300

    fix

--- utils/useResize.ts
+++ utils/useResize.ts
@@ -1,14 +1,14 @@
 import React from 'react'
 
 const useResize = () => {
-  const [width, setWidth] = React.useState<number | null>(1920)
+  const [width, setWidth] = React.useState<number>(1920)
 
   const handleResize = () => {
-    return setWidth(document.documentElement.clientWidth)
+    return setWidth(window.innerWidth)
   }
 
   React.useEffect(() => {
-    setWidth(document.documentElement.clientWidth)
+    setWidth(window.innerWidth)
     window.addEventListener('resize', handleResize)
     return () => {
       window.removeEventListener('resize', handleResize)
