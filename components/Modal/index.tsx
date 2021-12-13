fbafd1058240bff4c6d1ba815a4d2a9ad54e8465
voronkova.my <marina.v18@yandex.ru>
2021-12-13 18:05:39 +0300

    fix

--- components/Modal/index.tsx
+++ components/Modal/index.tsx
@@ -24,10 +24,19 @@ const Modal = ({ children, onClose, opened }: Props) => {
 
   React.useEffect(() => {
     if (opened) {
+      const div = document.createElement('div')
+      div.style.overflowY = 'scroll'
+      div.style.width = '50px'
+      div.style.height = '50px'
+      document.body.append(div)
+      const scrollWidth = div.offsetWidth - div.clientWidth
+      div.remove()
+      document.body.style.paddingRight = `${scrollWidth}px`
       document.body.style.overflow = 'hidden'
       document.addEventListener('keydown', handleKeyDown)
     }
     return () => {
+      document.body.style.paddingRight = '0'
       document.body.style.overflow = 'auto'
       document.removeEventListener('keydown', handleKeyDown)
     }
